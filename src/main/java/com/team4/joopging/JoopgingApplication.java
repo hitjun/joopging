cc12a7b0fb3426bd34b8d7a39c043172326c3082
by <daydayto@naver.com>
2021-10-12 00:25:27 +0900

    joopging 1

---
+++ src/main/java/com/team4/joopging/JoopgingApplication.java
@@ -0,0 +1,13 @@
+package com.team4.joopging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoopgingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoopgingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team4/joopging/JoopgingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.joopging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoopgingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
